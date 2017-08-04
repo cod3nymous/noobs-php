a138d18a37dd3e67518c73c09ec184490838bb72
cod3nymous <30438442+cod3nymous@users.noreply.github.com>
2017-08-04 13:34:27 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$a = 10;
+$b = 20;
+$c = 30;
+$num = array($a , $b , $c);
+echo max($num);
---
+++ php_cond.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 100;
+$b = 200;
+$c = 70;
+
+if ($a>$b && $a>$c)
+{
+echo ($a);
+}
+
+else if ($b>$a && $b>$c)
+{
+echo ($b);
+}
+
+else
+{
+echo ($c);
+}
